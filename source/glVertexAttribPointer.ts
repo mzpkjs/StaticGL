e2878b5b019844d14112d1b0749b857ec063b448
Mateusz Pietrzak <57403837+mzpkjs@users.noreply.github.com>
2020-06-16 10:13:10 +0200

    fix: Corrected the return type of "glVertexAttribPointer" function.

--- source/glVertexAttribPointer.ts
+++ source/glVertexAttribPointer.ts
@@ -2,7 +2,7 @@ import gl from "./webglContext"
 
 
 
-const glVertexAttribPointer = (index: GLuint, size: GLint, type: GLenum, normalized: GLboolean, stride: GLsizei, offset: GLintptr): never => {
+const glVertexAttribPointer = (index: GLuint, size: GLint, type: GLenum, normalized: GLboolean, stride: GLsizei, offset: GLintptr): void => {
     gl.vertexAttribPointer(index, size, type, normalized, stride, offset)
 }
 
